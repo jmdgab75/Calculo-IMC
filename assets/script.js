162d7a089ea162e97761cc6fe268c4dbec1847a6
Gabriel <gabriel.diniz@estudante.ifms.edu.br>
2023-07-11 14:57:44 -0400

    Commit Correções

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,23 @@
-function calculoIMC() {
-    let peso = parseFloat(document.getElementById("peso").value);
-    let altura = parseFloat(document.getElementById("altura").value);
-    let sexo = document.getElementById("sexo").value;
-    let resultadoElement = document.getElementById("resultado");
+function calcularPesoIdeal() {
+    var peso = parseFloat(document.getElementById("peso").value);
+    var altura = parseFloat(document.getElementById("altura").value);
+    var sexo = document.getElementById("sexo").value;
+    var resultadoElement = document.getElementById("resultado");
 
-    if (isNaN(peso) || isNaN(altura)){
-       resultadoElement.textContent = "Insira dados númericos válidos.";
-       return;
-    }
-
-    let calcularIMC ;
-    if(sexo === "f") {
-    pesoIdeal = 62.1 * altura -44.7;
-    } else if (sexo === "m"){
-    pesoIdeal = 72.7 * altura - 58;
-    } else {
-      resultadoElement.textContent = "Selecione um sexo";
+    if (isNaN(peso) || isNaN(altura)) {
+      resultadoElement.textContent = "Por favor, insira valores numéricos válidos.";
       return;
     }
-    resultadoElement.textContent = "Seu IMC é: " + calcularIMC.toFixed(2) + "kg.";
-}
 
+    var pesoIdeal;
+    if (sexo === "f") {
+      pesoIdeal = 62.1 * altura - 44.7;
+    } else if (sexo === "m") {
+      pesoIdeal = 72.7 * altura - 58;
+    } else {
+      resultadoElement.textContent = "Por favor, selecione um sexo válido.";
+      return;
+    }
+
+    resultadoElement.textContent = "Seu peso ideal e: " + pesoIdeal.toFixed(2) + " kg.";
+  }
